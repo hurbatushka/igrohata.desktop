c3a066544d9ed9b89afc93d6a7b7c964172b8bcc
иван Хурбатов <hurbatushka@gmail.com>
2023-06-18 17:35:27 +0300

    Косметические изменения, вывод страницы теперь по центру экрана и в целом все

--- Windows/Window2.xaml.cs
+++ Windows/Window2.xaml.cs
@@ -17,9 +17,6 @@ using System.Windows.Shapes;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Логика взаимодействия для Window2.xaml
-    /// </summary>
     public partial class Window2 : Window
     {
         public Window2()
